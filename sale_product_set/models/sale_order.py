e8fff7f1155d7ab857d0df8b3a460060dcb27b24
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-11-12 19:25:52 +0200

    Remove deprecated files and add new purchase layout functionality
    Remove unused XML templates and `.gitignore` entries. Refactor multiple models, simplifying fields and methods for better maintainability. Introduce a new module, `purchase_layout_category_hide_detail`, to manage purchase order line visibility and layouts. Add configuration and usage documentation for the new module.

--- sale_product_set/models/sale_order.py
+++ sale_product_set/models/sale_order.py
@@ -1,8 +1,7 @@
 #  Part of Odoo. See LICENSE file for full copyright and licensing details.
 import logging
 
-from odoo import api, fields, models, SUPERUSER_ID, _, Command
-from odoo.tests import savepoint_seq
+from odoo import api, fields, models, _
 from odoo.tools import groupby
 
 _logger = logging.getLogger(__name__)
